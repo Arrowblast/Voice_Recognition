2404176798b602f9bbcca0aa6ae90e5ab70441c7
Wojciech Jaroński <wojtasj1996@gmail.com>
2017-12-21 11:51:46 +0100

    Modify data spliting into smaller blocks, and estimate result

--- Voice/voice.py
+++ Voice/voice.py
@@ -35,30 +35,61 @@ def hps(data, fs,range):
     i_peak = argmax(hps[:len(dec)])
     i_interp=parabolic(hps[:len(dec)],i_peak)[0]
     wynik=fs * i_interp / n
-    print(wynik)
+    # print(wynik)
     return wynik
+
+ilosc_blokow = 5
 
 files=glob.glob("train/train/*.wav")
 ile=0
-for f in files:
+print("_\t_\t_\tK\tM\t~\t")
+for (id, f) in enumerate(files):
     data, fs = sf.read(f)
     real_value=f.split('_')[1].split(".")[0]
-    range=5
-    wynik=1000
-    roz="unknown"
-    for range in np.arange(5,7):
-        wynik = hps(data, fs,range)
-        if(wynik>85 and wynik<180):
-            roz="M"
-            break
-        elif(wynik>165 and wynik<255):
-            roz="K"
-            break;
-        else:
-            roz="unknown"
+    n = int(len(data)/ilosc_blokow)
 
-    print(real_value+" "+roz)
+
+    iloscM = 0
+    iloscK = 0
+    iloscU = 0
+    for i in range(ilosc_blokow):
+        dataBlok = data[i*n:(i+1)*n]
+
+        range_2=5
+        wynik=1000
+        roz="unknown"
+        for range_2 in np.arange(5, 7):
+            wynik = hps(dataBlok, fs, range_2)
+            if(wynik>85 and wynik<180):
+                roz="M"
+                break
+            elif(wynik>165 and wynik<255):
+                roz="K"
+                break
+            else:
+                roz="unknown"
+        if(roz=='M'):
+            iloscM = iloscM+1
+
+        elif(roz=='K'):
+            iloscK = iloscK +1
+        else:
+            iloscU = iloscU + 1
+        if(iloscM > ilosc_blokow/2 or iloscK > ilosc_blokow/2):
+            break
+    if(iloscK > iloscM):
+        roz = "K"
+    elif(iloscM > iloscK):
+        roz = "M"
+    else:
+        roz = "U"
+
+    # print(real_value+" "+roz)
     if(real_value==roz):
         ile+=1
+        zgodnosc = "+"
+    else:
+        zgodnosc = "---"
+    print(str(id) + "\t" + real_value + "\t" + roz + "\t" + str(iloscK) + "\t" + str(iloscM) + "\t" + str(iloscU) + "\t" + zgodnosc)
 print(ile)
-
+print("zgodnosc: " + str(ile) + ", "+ str(ile/len(files)*100)+"%")
